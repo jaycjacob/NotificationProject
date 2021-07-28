a7206730ef55f42bcbdc0b7027e99d8dc1979768
Jacob Kipchirchir <jaycjacob@gmail.com>
2021-07-28 14:58:29 +0300

    Second commit

--- main.py
+++ main.py
@@ -1,32 +1,55 @@
-import datetime #for reading present date
+import sys
+import tkinter as tk
 import time
-from plyer import notification #for getting notification on PC
+import datetime
+from plyer import notification
 
 title = 'Notification App!'
-message= ''
 time_mes = {}
 
-count = 0
 
-def pop_not():
-    notification.notify(title= title,
-                    message=message,
-                    app_icon = None,
-                    timeout= 10,
-                    toast=False)
-    time.sleep(25)
+def notify_func():
+    while True:
+        now = datetime.datetime.now()
+        current_time = now.strftime('%H:%M:%S')
+        now_time = str(current_time)[0:5]
+        if now_time in time_mes:
+            notification.notify(title=title,
+                                message=time_mes[now_time],
+                                app_icon=None,
+                                timeout=10,
+                                toast=False)
+            time.sleep(25)
 
 
-while count<2:
-    x = str(input("Enter time in the format HHTT"))
-    y = input("Enter message to be displayed")
-    time_mes[x] = y
-    count += 1
+master = tk.Tk()
+master.title("set notification")
+tk.Label(master, text='Time for notification').grid(row=0)
+tk.Label(master, text='Notification message').grid(row=1)
+user_input = tk.Entry(master)
+user_input2 = tk.Entry(master)
+user_input.grid(row=0, column=1)
+user_input2.grid(row=1, column=1)
+ttt = str(user_input.get())
 
-while True:
-    now = datetime.datetime.now()
-    current_time = now.strftime('%H:%M:%S')
-    now_time = str(current_time)[0:5]
-    if now_time in time_mes:
-        message = time_mes[now_time]
-        pop_not()
+
+def set_notification():
+    t1 = user_input.get()
+    n1 = user_input2.get()
+    time_mes[t1] = n1
+
+
+tk.Button(master, text='Quit', command=master.quit and sys.exit).grid(row=3,
+                                                                      column=0,
+                                                                      sticky=tk.W,
+                                                                      pady=4)
+tk.Button(master, text='Notify', command=notify_func).grid(row=3,
+                                                           column=2,
+                                                           sticky=tk.W,
+                                                           pady=4)
+tk.Button(master, text='Set', command=set_notification).grid(row=2,
+                                                             column=1,
+                                                             sticky=tk.W,
+                                                             pady=4)
+
+master.mainloop()
