9ee6aeca04c3ab1abcf40aedd8366cafda9d57a4
Jacob Kipchirchir <jaycjacob@gmail.com>
2021-08-04 18:13:17 +0300

    Readable

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from plyer import notification
 
 title = 'Notification App'
 time_mes = {}
-bs = []
+time_mes_list = []
 running = True
 
 
@@ -31,23 +31,22 @@ def notify_func():
 
 # Set the condition if not true and enter time and notification message to the dictionary
 def set_notification():
-    global bs
-    t1 = user_input.get()
+    global time_mes_list
+    time_input = user_input.get()
+    mes_input = user_input2.get()
     # validating time
     try:
-        t = t1.split(':')
-        print(t[0])
-        index_0 = int(t[0])
-        index_1 = int(t[1])
+        time_split = time_input.split(':')
+        index_0 = int(time_split[0])
+        index_1 = int(time_split[1])
         if 24 > index_0 >= 0 and 60 > index_1 >= 0:
-            n1 = user_input2.get()
-            time_mes[t1] = n1
-            bs = list(time_mes.items())
+            time_mes[time_input] = mes_input
+            time_mes_list = list(time_mes.items())
             add_listbox()
         else:
             print("Wrong time")
     except ValueError:
-        print("Please input time")
+        print("Please input correct time")
         return False
     except IndexError:
         print("Please input HH:MM format")
@@ -61,20 +60,20 @@ def stop():
 
 
 def add_listbox():
-    global bs
+    global time_mes_list
     global var
-    var.set(bs)
+    var.set(time_mes_list)
 
 
 def delete():
-    global bs
+    global time_mes_list
     # index of the selected item in the listbox
-    index = lb.curselection()
+    index = listbox.curselection()
     # then find the item in the list
-    list_item = bs[index[0]]
+    list_item = time_mes_list[index[0]]
     # delete in the dictionary
     del time_mes[list_item[0]]
-    bs.remove(list_item)
+    time_mes_list.remove(list_item)
     add_listbox()
 
 
@@ -106,9 +105,9 @@ tk.Label(master, text='Notifications').grid(row=0,
 
 var = tk.StringVar()
 
-lb = tk.Listbox(master, listvariable=var)
-lb.grid(row=1,
-        column=3)
+listbox = tk.Listbox(master, listvariable=var)
+listbox.grid(row=1,
+             column=3)
 
 tk.Button(master, text="Delete", command=delete).grid(row=3,
                                                       column=4,
